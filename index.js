5d313a1eb1bbe5c45fa00b1b3046afae78db95d2
Md. Likhon Mia <likhon15-3916@diu.edu.bd>
2024-08-24 02:40:58 -0700

    added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://aspr.vercel.app', // Update this with your client URL
+    origin: 'https://aspr.vercel.app', // Update this with your client URL
     allowedHeaders: ['Authorization', 'Content-Type'],
 }));
 
